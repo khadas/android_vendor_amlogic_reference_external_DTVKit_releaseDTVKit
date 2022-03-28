e9085997aefa787c14d33fcef695fee6ed152ff9
Rongsheng Jiang <rongsheng.jiang@amlogic.com>
2022-03-28 13:20:15 +0800

    DTVKITRelease: add file [1/1]
    PD#SWPL-70133
    
    Problem:
    Android T Dtvkit Build
    
    Solution:
    add it
    
    Verify:
    T5D
    
    Change-Id: I55b31811725456663266ed3ca5731d7b2ac0c8ff
    Signed-off-by: Rongsheng Jiang <rongsheng.jiang@amlogic.com>

---
+++ Android.mk
@@ -0,0 +1,95 @@
+LOCAL_PATH := $(call my-dir)
+TA_UUID := 1987f328-1755-9321-5441-43494343544c
+TA_SUFFIX := .ta
+
+ifeq ($(PLATFORM_TDK_VERSION), 38)
+	ifeq ($(BOARD_AML_SOC_TYPE),)
+		LOCAL_TA := ta/v3/signed/$(TA_UUID)$(TA_SUFFIX)
+	else
+		LOCAL_TA := ta/v3/dev/$(BOARD_AML_SOC_TYPE)/$(TA_UUID)$(TA_SUFFIX)
+	endif
+else
+	LOCAL_TA := ta/v2/signed/$(TA_UUID)$(TA_SUFFIX)
+endif
+
+ifeq (,$(wildcard $(LOCAL_PATH)/../android-rpcservice))
+include $(CLEAR_VARS)
+LOCAL_MODULE := dtvkitserver_releaseinfo.txt
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc
+LOCAL_VENDOR_MODULE := true
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRELINK_MODULE := false
+LOCAL_VENDOR_MODULE := true
+LOCAL_STRIP_MODULE := false
+LOCAL_SHARED_LIBRARIES := \
+    android.hidl.allocator@1.0 \
+    libam_adp \
+    libam_mw \
+    libamdvr \
+    libbase \
+    libbinder \
+    libc++ \
+    libc \
+    libcrypto \
+    libcurl \
+    libcutils \
+    libdl \
+    libft2-aml \
+    libhidlbase \
+    libhidlmemory \
+    libhidltransport \
+    libjpeg \
+    liblog \
+    libm \
+    libmediahal_resman \
+    libmediahal_tsplayer \
+    libmediandk \
+    libsqlite \
+    libssl \
+    libutils \
+    vendor.amlogic.hardware.dtvkitserver@1.0 \
+    libaml_mp_sdk.vendor \
+    libutilscallstack \
+    libteec \
+    libsecmem \
+    libsystemcontrolservice \
+    vendor.amlogic.hardware.systemcontrol@1.0 \
+    vendor.amlogic.hardware.systemcontrol@1.1
+
+LOCAL_MODULE := dtvkitserver
+LOCAL_SRC_FILES := dtvkitserver
+LOCAL_INIT_RC := dtvkitserver.rc
+LOCAL_VINTF_FRAGMENTS := vendor.amlogic.hardware.dtvkitserver@1.0.xml
+LOCAL_MODULE_RELATIVE_PATH := hw
+include $(BUILD_PREBUILT)
+endif
+
+$(warning CIPLUS_TA:$(LOCAL_TA) )
+ifeq ($(LOCAL_PATH)/$(LOCAL_TA), $(wildcard $(LOCAL_PATH)/$(LOCAL_TA)))
+$(warning Install CIPLUS_TA:$(LOCAL_TA) )
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(LOCAL_TA)
+LOCAL_MODULE := $(TA_UUID)
+LOCAL_MODULE_SUFFIX := $(TA_SUFFIX)
+LOCAL_STRIP_MODULE := false
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/teetz
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := tee_ciplus_ta
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_REQUIRED_MODULES := $(TA_UUID)
+include $(BUILD_PHONY_PACKAGE)
+endif
