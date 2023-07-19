5cb2a8a38e0c6be44fcc2807905614995f25c564
huimin.wang <huimin.wang@amlogic.com>
2023-07-19 15:54:14 +0800

    releaseDTVKit: remove ta [5/7]
    PD#SWPL-127341
    
    Problem:
    add CIPLUS ccta
    
    Solution:
    remove ta
    
    Verify:
    T5D
    
    Change-Id: I25e5023da133efca225ac51935f8244241dadf1e
    Signed-off-by: huimin.wang <huimin.wang@amlogic.com>

--- Android.mk
+++ Android.mk
@@ -1,16 +1,4 @@
 LOCAL_PATH := $(call my-dir)
-TA_UUID := 1987f328-1755-9321-5441-43494343544c
-TA_SUFFIX := .ta
-
-ifeq ($(PLATFORM_TDK_VERSION), 38)
-	ifeq ($(BOARD_AML_SOC_TYPE),)
-		LOCAL_TA := ta/v3/signed/$(TA_UUID)$(TA_SUFFIX)
-	else
-		LOCAL_TA := ta/v3/dev/$(BOARD_AML_SOC_TYPE)/$(TA_UUID)$(TA_SUFFIX)
-	endif
-else
-	LOCAL_TA := ta/v2/signed/$(TA_UUID)$(TA_SUFFIX)
-endif
 
 ifeq (,$(wildcard $(LOCAL_PATH)/../android-rpcservice))
 include $(CLEAR_VARS)
@@ -77,21 +65,4 @@ LOCAL_MULTILIB := 32
 include $(BUILD_PREBUILT)
 endif
 
-ifeq ($(LOCAL_PATH)/$(LOCAL_TA), $(wildcard $(LOCAL_PATH)/$(LOCAL_TA)))
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := $(LOCAL_TA)
-LOCAL_MODULE := $(TA_UUID)
-LOCAL_MODULE_SUFFIX := $(TA_SUFFIX)
-LOCAL_STRIP_MODULE := false
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/teetz
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := tee_ciplus_ta
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_REQUIRED_MODULES := $(TA_UUID)
-include $(BUILD_PHONY_PACKAGE)
-endif
+include $(call all-makefiles-under,$(LOCAL_PATH))
